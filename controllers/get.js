b1af4620bed78b9699523185b9c6f863565bb671
Sarah Riehl <sls.riehl@gmail.com>
2017-09-24 16:40:49 -0500

    get controller: rework promises to avoid nesting, add data to render instead of hard coded, use moment for yelp times

--- controllers/get.js
+++ controllers/get.js
@@ -2,14 +2,18 @@ const util = require('util');
 
 const axios = require('axios');
 
+const moment = require('moment');
+
 const yelpApi = require('./yelp');
 
-const helpers = require('./helpers');
+const about = require('../data/about');
+const team = require('../data/team');
+const homes = require('../data/homes');
 
 const controller = {
 	renderIndex: (req, res) => {
 		console.log('root fired');
-		yelpApi.getToken()
+		return yelpApi.getToken()
 		.then((data) => {
 			console.log('promise 1 fired');
 			console.log(data.data);
@@ -18,43 +22,50 @@ const controller = {
 					'Authorization': `Bearer ${data.data.access_token}`
 				}
 			}
-			axios.all([yelpApi.getBizInfo(access_token), yelpApi.getReviews(access_token)])
-			.then(axios.spread((bizInfo, reviewInfo) => {
-				console.log(`bizInfo ${util.inspect(bizInfo.data)}`);
-				console.log(`reviews ${util.inspect(reviewInfo.data)}`);
-				let openRes = bizInfo.data.hours[0].open;
-				let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-				let open = [];
-				for(let i in openRes) {
-					const hours = {
-						day: days[i],
-						open: helpers.timeConvert(openRes[i].start),
-						close: helpers.timeConvert(openRes[i].end)
-					}
-					open.push(hours);
-				}
-				let reviews = [];
-				for(let i in reviewInfo.data.reviews) {
-					let review = {
-						url, text, user, rating
-					} = reviewInfo.data.reviews[i];
-					review.time_created = moment(reviewInfo.data.reviews[i].time_created).format("MMM Do, YYYY h:mm a");
-					review.ratingArr = Array.from({length: Math.floor(reviewInfo.data.reviews[i].rating)})
-					reviews.push(review);
-				}
-				const data = {
-					open, reviews,
-					noRevs: bizInfo.data.review_count,
-					rating: bizInfo.data.rating,
-					ratingArr: Array.from({length: Math.floor(bizInfo.data.rating)}),
-					url: bizInfo.data.url
-				}
-				console.log(util.inspect(data));
-				res.render('index.hbs', {data});
-			}));
+			return axios.all([
+				yelpApi.getBizInfo(access_token),
+				yelpApi.getReviews(access_token)
+			])
 		})
+		.then(axios.spread((bizInfo, reviewInfo) => {
+			console.log(`bizInfo ${util.inspect(bizInfo.data)}`);
+			console.log(`reviews ${util.inspect(reviewInfo.data)}`);
+			let openRes = bizInfo.data.hours[0].open;
+			let open = [];
+			for(let i in openRes) {
+				const hours = {
+					day: moment(openRes[i].day, 'e').format('dddd'),
+					open: moment(openRes[i].start, 'HHmm').format('h:mm a'),
+					close: moment(openRes[i].end, 'HHmm').format('h:mm a')
+				}
+				open.push(hours);
+			}
+			let reviews = [];
+			for(let i in reviewInfo.data.reviews) {
+				let review = {
+					url, text, user, rating
+				} = reviewInfo.data.reviews[i];
+				review.time_created = moment(reviewInfo.data.reviews[i].time_created).format("MMM Do, YYYY h:mm a");
+				review.ratingArr = Array.from({length: Math.floor(reviewInfo.data.reviews[i].rating)})
+				reviews.push(review);
+			}
+			const hoursReviews = {
+				open,
+				reviews,
+				noRevs: bizInfo.data.review_count,
+				rating: bizInfo.data.rating,
+				ratingArr: Array.from({
+					length: Math.floor(bizInfo.data.rating)
+				}),
+				url: bizInfo.data.url
+			}
+
+			res.render('index.hbs', {
+				hoursReviews, about, team, homes
+			})
+		}))
 		.catch((err) => {
-			console.log(`catch err ${err}`);
+			console.log(err);
 		});
 	}
 }
