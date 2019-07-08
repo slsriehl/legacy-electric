86d5547db7b576c9528fec454048bb34f87eb44a
Sarah Schieffer Riehl <sls.riehl@gmail.com>
2019-07-08 17:37:08 -0500

    team bio, photo, view, and scss updates

--- data/team.js
+++ data/team.js
@@ -2,22 +2,28 @@
 
 const team = [{
 	img: "img/team/lane.jpg",
-	alt: "Lane Eskew",
+	alt: "Lane Eskew, Owner and Master Electrician",
 	name: "Lane Eskew",
 	title: "Owner and Master Electrician",
 	info: "Lane was born and raised in South Austin and nearby Driftwood. Both of his folks were born here as well and both sides have been in the Austin area for generations. During his summers and several Christmas vacations growing up, he worked for his old man at his electrical contracting company... and the rest is history. He started Legacy Electrical Services to serve his neighbors and fellow Austinites in the manner he believes they deserve."
 }, {
+	img: "img/team/david.jpg",
+	alt: "David Montelongo, Electrician",
+	name: "David Montelongo",
+	title: "Electrician",
+	info: "David is also a native, lifelong Austinite. He was introduced to the electrical contracting field while working in a call center, and the ability to be outdoors and moving around all day appealed to him. He has two sons, and enjoys hiking, kayaking, and playing drums."
+}, {
 	img: "img/team/candyce.jpg",
-	alt: "Candyce Eskew",
+	alt: "Candyce Eskew, Office Manager",
 	name: "Candyce Eskew",
 	title: "Office Manager",
 	info: "Lane's wife Candyce was born and raised in Austin and nearby Pflugerville. Her father's family has been in Austin for generations, and her mother's family moved here from West Texas. She earned a BBA in management from the University of Texas and enjoys being a mom to their two daughters."
 }, {
-	img: "img/team/employee.jpg",
-	alt: "Employee",
-	name: "Employee",
-	title: "Employee",
-	info: ""
-}]
+	img: "img/team/sara.jpg",
+	alt: "Sara Hendrickson, Administrative Assistant",
+	name: "Sara Hendrickson",
+	title: "Administrative Assistant",
+	info: "Sara was born on Homestead Air Force Base in Florida, lived in Germany for five years, and then her family was transferred to Austin, where they stayed after retirement from the military. She loves to read, watch movies, and enjoys spending time with her four children."
+}];
 
 module.exports = team;
